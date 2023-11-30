e947af061a712d95fc5a9581459fe57deb3980f3
Rochel <liuyingfeng57@gmail.com>
2023-11-30 14:54:37 +0800

    adjust

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rochelwang1205.github.io',
+	site: 'https://rochelwang1205/graduateBlog.github.io',
 	// integrations: [mdx(), sitemap()],
 });
